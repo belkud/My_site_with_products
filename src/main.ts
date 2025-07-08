cd8ce564e16c5746de4ce623c0e7ca679edaeb15
belkud <vlad-belkud@yandex.ru>
2025-07-09 01:32:33 +0300

    form for entrance

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+
+
+let entrance = document.querySelector('#entrance') as HTMLDivElement
+let userId = document.querySelector('#userId') as HTMLFormElement
+let cross = document.querySelector('#cross') as HTMLButtonElement
+
+cross.addEventListener('click',()=> {
+  userId.style.display='none'
+})
