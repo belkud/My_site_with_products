0419e7a19b59b0f27e9a9168c5ff104ca99ec83a
belkud <vlad-belkud@yandex.ru>
2025-07-09 10:07:17 +0300

    create overlay

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,27 @@ import './style.css'
 
 
 let entrance = document.querySelector('#entrance') as HTMLDivElement
-let userId = document.querySelector('#userId') as HTMLFormElement
+let overlay = document.querySelector('#overlay') as HTMLDivElement
+// let userId = document.querySelector('#userId') as HTMLFormElement
 let cross = document.querySelector('#cross') as HTMLButtonElement
 
+entrance.addEventListener('click',()=> {
+  overlay.style.display='block'
+})
+
 cross.addEventListener('click',()=> {
-  userId.style.display='none'
+  overlay.style.display='none'
+})
+
+overlay.addEventListener('click',(e:any)=> {
+  console.log(e.target.id);
+  
+  if (e.target.id=='userId' || e.target.id=='') {
+    e.preventDefault()
+    console.log('123');
+    
+    
+  } else {
+    overlay.style.display='none'
+  }
 })
