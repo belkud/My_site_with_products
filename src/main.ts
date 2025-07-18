59e17b147b2cffa0b8e62646a2748d39dbbf70c6
belkud <vlad-belkud@yandex.ru>
2025-07-18 16:31:10 +0300

    registration_form

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,34 @@ import './style.css'
 
 let entrance = document.querySelector('#entrance') as HTMLDivElement
 let overlay = document.querySelector('#overlay') as HTMLDivElement
-// let userId = document.querySelector('#userId') as HTMLFormElement
 let cross = document.querySelector('#cross') as HTMLButtonElement
+let button_registration = document.querySelector('#button_registration') as HTMLDivElement
+let form_entrance = document.querySelector('#form_entrance') as HTMLFormElement
+let form_registration = document.querySelector('#form_registration') as HTMLFormElement
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
 
 entrance.addEventListener('click',()=> {
-  overlay.style.opacity='0'
-  overlay.style.display='block'
-  setTimeout(() => {
+    form_registration.style.display='none'
+    form_entrance.style.display='block'
+    overlay.style.opacity='0'
+    overlay.style.display='block'
+    setTimeout(() => {
     overlay.style.opacity='1'
-    }, 500);
+  }, 500);
 })
 
 cross.addEventListener('click',()=> {
-      overlay.style.opacity='0'
+    overlay.style.opacity='0'
     setTimeout(() => {
-      overlay.style.display='none'
-    }, 500);
+    overlay.style.display='none'
+  }, 500);
 })
 
 overlay.addEventListener('click',(e:any)=> {
-  console.log(e.target.id);
+  // console.log(e.target.id);
   
   if (e.target.id=='userId' || e.target.id=='') {
     return
@@ -33,3 +41,33 @@ overlay.addEventListener('click',(e:any)=> {
     }, 500);
   }
 })
+
+button_registration.addEventListener('click',()=> {
+    overlay.style.opacity='0'
+    overlay.style.display='block'
+    form_registration.style.display='block'
+    form_entrance.style.display='none'
+    setTimeout(() => {
+    overlay.style.opacity='1'
+  }, 500);
+
+
+})
+
+
+})
+
+
+
+
+// let test_button = document.querySelector('#test_button') as any
+// test_button.addEventListener('click', ()=> {
+//   console.log('yyyyyyyyyy');
+  
+// })
+
+
+
+
+
+
