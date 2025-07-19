9c23bfe92afc7cfef2f95101222872b4e582eafe
belkud <vlad-belkud@yandex.ru>
2025-07-19 17:43:00 +0300

    passwords

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import './style.css'
 
 
-let entrance = document.querySelector('#entrance') as HTMLDivElement
-let overlay = document.querySelector('#overlay') as HTMLDivElement
-let cross = document.querySelector('#cross') as HTMLButtonElement
-let button_registration = document.querySelector('#button_registration') as HTMLDivElement
-let form_entrance = document.querySelector('#form_entrance') as HTMLFormElement
-let form_registration = document.querySelector('#form_registration') as HTMLFormElement
+let entrance = document.querySelector('#entrance') as HTMLInputElement //! кнопка входа
+let overlay = document.querySelector('#overlay') as HTMLDivElement //! подложка
+let cross = document.querySelector('#cross') as HTMLButtonElement //! кнопка закрытия формы
+let registration = document.querySelector('#registration') as HTMLDivElement
+let form_entrance = document.querySelector('#form_entrance') as HTMLFormElement //! форма с логином и паролем
+let form_registration = document.querySelector('#form_registration') as HTMLFormElement //!регистрация пользователя
+
+
+let registration_button = document.querySelector('.registration_button') as HTMLDivElement
+let enter_password = document.querySelector('#enter_password') as HTMLDivElement //!div всплывает при неверных паролях
+let wrong_password = document.querySelector('#wrong_password') as HTMLDivElement //!div всплывает при неверных паролях
 
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
 
 entrance.addEventListener('click',()=> {
     form_registration.style.display='none'
@@ -32,7 +36,7 @@ cross.addEventListener('click',()=> {
 overlay.addEventListener('click',(e:any)=> {
   // console.log(e.target.id);
   
-  if (e.target.id=='userId' || e.target.id=='') {
+  if (e.target.id=='userId' || e.target.id=='wrong_password' || e.target.id=='form_registration' || e.target.id=='form_entrance' || e.target.id=='') {
     return
   } else {
     overlay.style.opacity='0'
@@ -42,32 +46,40 @@ overlay.addEventListener('click',(e:any)=> {
   }
 })
 
-button_registration.addEventListener('click',()=> {
-    overlay.style.opacity='0'
+registration.addEventListener('click',()=> {
+  overlay.style.opacity='0'
     overlay.style.display='block'
     form_registration.style.display='block'
     form_entrance.style.display='none'
     setTimeout(() => {
     overlay.style.opacity='1'
   }, 500);
-
-
-})
-
-
 })
 
 
 
+let inputs = form_registration.getElementsByTagName('input')
+registration_button.addEventListener('click', (e)=> {  
+  if (inputs[1].value=='') {
+    enter_password.style.display = 'block'
+    e.preventDefault()
+  } else if (inputs[1].value !=inputs[2].value) {
+    wrong_password.style.display = 'block'
+    enter_password.style.display = 'none'
+    e.preventDefault()
+  }else {
+    wrong_password.style.display = 'none'
+    enter_password.style.display = 'none'
+  //   overlay.style.opacity='0'
+  //   setTimeout(() => {
+  //   overlay.style.display='none'
+  // }, 500);
+  }
+})
 
-// let test_button = document.querySelector('#test_button') as any
-// test_button.addEventListener('click', ()=> {
-//   console.log('yyyyyyyyyy');
+
+document.body.addEventListener('click', (e)=> {
+  console.log(e.target);
   
-// })
-
-
-
-
-
-
+})
+console.log(inputs[0]);
