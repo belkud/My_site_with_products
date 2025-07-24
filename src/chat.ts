d66476c378276648872b5c82786258541c0358d1
belkud <vlad-belkud@yandex.ru>
2025-07-24 12:00:20 +0300

    settings

--- src/chat.ts
+++ src/chat.ts
@@ -3,63 +3,22 @@ import './style.css'
 
 
 
-console.log(document.cookie);
-console.log(document.cookie);
-
-let chat_with_users = document.querySelector('#chat_with_users') as HTMLTextAreaElement
-
-document.body.addEventListener('input', ()=> {
-    document.cookie = `${chat_with_users.value};`
-})
-
-chat_with_users.value = document.cookie
 
 // console.log(document.cookie);
 
+window.addEventListener('DOMContentLoaded', ()=> {
+    
+    console.log(document.cookie);
+    console.log(document.cookie);
+    
+    let chat_with_users = document.querySelector('#chat_with_users') as HTMLTextAreaElement
+    
+    document.body.addEventListener('input', ()=> {
+        document.cookie = `${chat_with_users.value};`
+    })
+    
+    chat_with_users.value = document.cookie
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log('qwerty');
-
-
+})
 
