31a3bd3019497560c729bc5b91369b5bae3d7e2d
belkud <vlad-belkud@yandex.ru>
2025-07-24 11:53:00 +0300

    config3

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 export default {
     base: '/My_site_with_products/',
-    base2: '/My_site_with_products/chat.html'
-    }
+    base2: '/My_site_with_products/chat.html',
     
+    server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
+}
