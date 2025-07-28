b0dc7558b9521c87346a7e9dccc97497dd8e6071
belkud <vlad-belkud@yandex.ru>
2025-07-28 21:20:34 +0300

    container_with_tickets2

--- src/chat.ts
+++ src/chat.ts
@@ -69,6 +69,7 @@ console.log(cookArray);
 let seats = document.querySelector('#seats') as HTMLDivElement
 let tickets_chart = document.querySelector('#tickets_chart') as HTMLDivElement
 let book = document.querySelector('#book') as HTMLInputElement
+
 // let passangers_info = document.querySelector('#passangers_info') as HTMLDivElement
 
 // console.log(passangers_info);
@@ -79,19 +80,19 @@ for (let i = 0; i < seats.children.length; i++) {
   seats.children[i].innerHTML +=`
       <div>
       <input type="checkbox">
-      <label for=""></label>
+      <label></label>
       </div>
       <div>
         <input type="checkbox">
-        <label for=""></label>
+        <label></label>
         </div>
       <div>
         <input type="checkbox">
-        <label for=""></label>
+        <label></label>
         </div>
       <div>
       <input type="checkbox">
-        <label for=""></label>
+        <label></label>
       </div>
       `
       }
@@ -103,22 +104,40 @@ let tickets_label = seats.getElementsByTagName('label')
 let tickets_input = seats.getElementsByTagName('input')
 
 for (let i = 0; i < tickets_label.length; i++) {
-
+  
   tickets_input[i].name  = String(i+1)
-  tickets_label[i].textContent  = String(i +1)
+  tickets_label[i].innerText  = String(i +1)
+  
+  
+  
 }
 
+let data = document.querySelector('#data') as HTMLInputElement
+data.value = '2025-05-07'
 
+let directions = document.querySelector('#directions') as HTMLSelectElement
+ 
+console.log(data);
 
 book.addEventListener('click', (e)=> {
-
-  
   e.preventDefault()
+  
+  for (let i = 0; i < tickets_input.length; i++) {
+      if (tickets_input[i].checked == true) {
   tickets_chart.innerHTML+= `<div>
-      <div>Moscow</div>
-      <div>27.02.2025</div>
-      <div>3</div>
-    </div>`
+  <div>${directions.value}</div>
+  <div>${data.value}</div>
+  <div>${tickets_input[i].name}</div>
+  </div>`
+  // tickets_input[i].remove()
+} 
+ 
+
+
+          
+  }
+
+ 
 
 })
 
