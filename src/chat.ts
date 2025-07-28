a57d1fd79a93f90f4e83f3495654bf8e998eba52
belkud <vlad-belkud@yandex.ru>
2025-07-28 20:32:10 +0300

    container_with_tickets

--- src/chat.ts
+++ src/chat.ts
@@ -33,14 +33,103 @@ container[3].addEventListener('click', ()=> {
   
 let cookArray = document.cookie.split(',')
 for (let i = 0; i < container.length-1; i++) {
-  if (cookArray[i].includes('label')) {
-        console.log(cookArray[i]);
-        container[i].value = `${cookArray[i].replace(`label[${i}]=`, '')}`  
-  }
+  // if (cookArray[i].includes('label')) {
+  //       console.log(cookArray[i]);
+  //       container[i].value = `${cookArray[i].replace(`label[${i}]=`, '')}`  
+  // }
 } 
 
     
 console.log(cookArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let seats = document.querySelector('#seats') as HTMLDivElement
+let tickets_chart = document.querySelector('#tickets_chart') as HTMLDivElement
+let book = document.querySelector('#book') as HTMLInputElement
+// let passangers_info = document.querySelector('#passangers_info') as HTMLDivElement
+
+// console.log(passangers_info);
+
+
+
+for (let i = 0; i < seats.children.length; i++) {
+  seats.children[i].innerHTML +=`
+      <div>
+      <input type="checkbox">
+      <label for=""></label>
+      </div>
+      <div>
+        <input type="checkbox">
+        <label for=""></label>
+        </div>
+      <div>
+        <input type="checkbox">
+        <label for=""></label>
+        </div>
+      <div>
+      <input type="checkbox">
+        <label for=""></label>
+      </div>
+      `
+      }
+
+
+
+
+let tickets_label = seats.getElementsByTagName('label')
+let tickets_input = seats.getElementsByTagName('input')
+
+for (let i = 0; i < tickets_label.length; i++) {
+
+  tickets_input[i].name  = String(i+1)
+  tickets_label[i].textContent  = String(i +1)
+}
+
+
+
+book.addEventListener('click', (e)=> {
+
+  
+  e.preventDefault()
+  tickets_chart.innerHTML+= `<div>
+      <div>Moscow</div>
+      <div>27.02.2025</div>
+      <div>3</div>
+    </div>`
+
+})
+
+
+
+
+
+
+
+
+
 
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 export default {
     base: '/My_site_with_products/',
-    build: {
-      rollupOptions: {
-        input: {
-          // @ts-ignore
-          main: resolve(__dirname, 'index.html'),
-          // @ts-ignore
-          idbm: resolve(__dirname, 'chat.html'),
-        }
-      }
-    }
+    // build: {
+    //   rollupOptions: {
+    //     input: {
+    //       // @ts-ignore
+    //       main: resolve(__dirname, 'index.html'),
+    //       // @ts-ignore
+    //       idbm: resolve(__dirname, 'chat.html'),
+    //     }
+    //   }
+    // }
   }
